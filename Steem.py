50d3fecd42eeaee905083f4295bd856226fd5530
FahadAlsaadi99 <fa.alsaadi99@gmail.com>
2024-09-02 01:07:46 +0300

    Update Steem.py

--- Steem.py
+++ Steem.py
@@ -4,20 +4,22 @@ import pandas as pd
 
 st.title("E-Sport")
 
-
 Processor = st.number_input("CPU(Core) ", min_value=0, max_value=24, value=2)
 Memory = st.number_input("Memory(GB) ", min_value=0, max_value=8192, value=2)
 Storage = st.number_input("Storage(GB)", min_value=0, max_value=30000, value=15000)
 
 df = pd.read_csv('list_of_games.csv')
 if st.button("Predict"):
-    response = requests.post(f"https://e-sport-fastapi.onrender.com/predict", json={
+    response = requests.post("https://e-sport-fastapi.onrender.com/predict", json={
         "Processor": Processor,
         "Memory": Memory,
         "Storage": Storage,
     })
     
-   if prediction == [1]:
+    if response.status_code == 200:
+        prediction = response.json().get("cluster")
+        
+        if prediction == [1]:
             subset = df[df['cluster'] == 1]['Game Names']
             sample_size = min(10, len(subset))  # تعديل العدد هنا إلى 10
             a = subset.sample(n=sample_size)  # إزالة random_state لجعل العينة عشوائية
@@ -30,4 +32,3 @@ if st.button("Predict"):
             st.write(b)
     else:
         st.write("Error: Could not get prediction from the API")
-
