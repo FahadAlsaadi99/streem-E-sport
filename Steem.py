9a6e1a1e3bbaf97151c3e1c6b726f2ccc1d788d6
FahadAlsaadi99 <fa.alsaadi99@gmail.com>
2024-09-02 01:03:39 +0300

    Update Steem.py

--- Steem.py
+++ Steem.py
@@ -19,16 +19,16 @@ if st.button("Predict"):
     
     if response.status_code == 200:
         prediction = response.json().get("cluster")
-        #st.write(f"The predicted cluster is: {prediction}")
-       if prediction == [1]:
+        
+        if prediction == [1]:
             subset = df[df['cluster'] == 1]['Game Names']
-            sample_size = min(10, len(subset)) 
+            sample_size = min(10, len(subset))  # تعديل العدد هنا إلى 10
             a = subset.sample(n=sample_size, random_state=1)
             st.write(a)
         
         if prediction == [0]:
             subset = df[df['cluster'] == 0]['Game Names']
-            sample_size = min(10, len(subset)) 
+            sample_size = min(10, len(subset))  # تعديل العدد هنا إلى 10
             b = subset.sample(n=sample_size, random_state=1)
             st.write(b)
     else:
