2362d2cb985a4303b3d29227390686e834cfc1d0
FahadAlsaadi99 <fa.alsaadi99@gmail.com>
2024-09-02 01:11:31 +0300

    Update Steem.py

--- Steem.py
+++ Steem.py
@@ -23,7 +23,8 @@ if st.button("Predict"):
             subset = df[df['cluster'] == 1]['Game Names']
             sample_size = min(10, len(subset))
             a = subset.sample(n=sample_size) 
-            st.write(a.to_frame().reset_index(drop=True))
+            shtml_table = a.to_frame().reset_index(drop=True).to_html(index=False)
+            st.markdown(html_table, unsafe_allow_html=True)
         
         if prediction == [0]:
             subset = df[df['cluster'] == 0]['Game Names']
